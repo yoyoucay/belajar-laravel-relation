c8cfaf591ab1d85550920df62f367928d964f847
Beemons <beemons123@gmail.com>
2019-01-31 14:40:29 +0700

    Intro dan persiapan

--- database/migrations/2019_01_31_071730_create_passports_table.php
+++ database/migrations/2019_01_31_071730_create_passports_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreatePassportsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('passports', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('no_pass', 100);
+            $table->integer('user_id')->unsigned();
             $table->timestamps();
+
+            $table->foreign('user_id')->refrences('id')
+                  ->on('users')->onDelete('cascade');
         });
     }
 
@@ -27,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('passports');
     }
 }
